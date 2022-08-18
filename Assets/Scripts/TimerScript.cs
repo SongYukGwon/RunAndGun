aadffec458838424f0f35a34d2e89111302179b1
Goldrichs <thddbr1254@naver.com>
2022-08-19 00:53:01 +0900

    make timer
    need to mediate balance and optimize

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+
+    float sec;
+    int min;
+
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+    }
+
+    private void Timer()
+    {
+        sec += Time.deltaTime;
+
+        timerText.text = string.Format("{0:D2}:{1:D2}", min, (int)sec);
+
+        if((int)sec > 59)
+        {
+            sec = 0;
+            min++;
+        }
+
+    }
+}
